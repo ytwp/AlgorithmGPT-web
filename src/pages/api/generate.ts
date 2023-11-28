6cff523d44f6302280e2a8a19a093d319a4986ad
ytwp <ytwppp@163.com>
2023-11-28 10:06:46 +0800

    1

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -7,7 +7,7 @@ import type { APIRoute } from 'astro'
 
 const apiKey = import.meta.env.OPENAI_API_KEY
 const httpsProxy = import.meta.env.HTTPS_PROXY
-const baseUrl = ((import.meta.env.OPENAI_API_BASE_URL) || 'https://api.openai.com').trim().replace(/\/$/, '')
+const baseUrl = 'https://algorithm.flyingduck.cn'
 const sitePassword = import.meta.env.SITE_PASSWORD || ''
 const passList = sitePassword.split(',') || []
 
